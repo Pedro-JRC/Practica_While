7caa6b8d9072ff044dd3fc1261cf378a36bcdc62
Pedro J. Rosario <131802637+Pedro-JRC@users.noreply.github.com>
2024-11-22 22:37:06 -0400

    Add project files.

---
+++ Calificaciones_While/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Calificaciones_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+            string cantDigita = string.Empty;
+            int cantCalif = 0;
+            string califDigita = string.Empty;
+            int calificaciones = 0;
+            int aprobados = 0;
+            int reprobados = 0;
+            int valor = 0; 
+
+            try
+            {
+
+                // SOLICITA LA CANTIDAD DE CALIFICACIONES A EVALUAR
+                Console.WriteLine("Favor indique la cantidad de calificaciones a evaluar.");
+                cantDigita = Console.ReadLine();
+
+                // SEPARADOR
+                Console.WriteLine();
+
+                // VALIDACIONES
+                if (int.TryParse(cantDigita, out cantCalif) && cantCalif > 0)
+                {
+
+                    // BUCLE PARA SOLICITAR LAS NOTAS
+                    while (valor++ < cantCalif)
+                    {
+                        // SOLICITA LAS NOTAS
+                        Console.Write($"Favor ingrese la nota {valor}: ");
+                        califDigita = Console.ReadLine();
+                        
+                        // SEPARADOR
+                        Console.WriteLine();
+
+                        // VALIDACIONES
+                        if (int.TryParse(califDigita, out calificaciones) && calificaciones >= 7)
+                        {
+                            // INNCREMENTA CANTIDAD DE APROBADOS SI LA NOTA ES MAYOR O IGUAL A 7
+                            aprobados++;
+                        }
+                        else if (int.TryParse(califDigita,out calificaciones) && calificaciones < 7)
+                        {
+                            // INCREMENTA CANTIDAD DE REPROBADOS SI LA NOTA ES MENOR A 7
+                            reprobados++;
+                        }
+                        else
+                        {
+                            // MENSAJE DE ERROR EN CASO SE INTRODUZCAN DATOS INVALIDOS
+                            Console.WriteLine("ERROR: Favor ingrese valores validos.");
+                        }
+                        
+                        
+                    }
+                }
+                else
+                {
+                    // MENSAJE DE ERROR EN CASO SE INTRODUZCAN DATOS INVALIDOS
+                    Console.WriteLine("ERROR: Favor ingrese valores validos.");
+                }
+
+                // MUESTRA LA CANTIDAD DE ESTUDIANTES QUE ALCAZARON 7 O MAS Y LA CANTIDAD QUE SACO MENOS DE 7
+                Console.WriteLine($"Estudiantes que sacaron 7 o mas: ({aprobados})");
+                Console.WriteLine($"Estudiantes que sacaron menos de 7: ({reprobados})");
+            }
+            catch
+            {
+                // MENSAJE DE ERROR EN CASO SE INTRODUZCAN DATOS INVALIDOS
+                Console.WriteLine("ERROR: Favor ingrese valores validos.");
+            }
+        }
+    }
+}
---
+++ Nomina_While/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Nomina_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+            string cantEmpleadoDigita = string.Empty;
+            int cantEmpleados = 0;
+            string sueldoDigita = string.Empty;
+            int sueldo = 0;
+            int menor300 = 0;
+            int mayor300 = 0;
+            int gastNomina = 0;
+            int valorEmp = 0;
+
+            try
+            {
+                // SOLICITA LA CANTIDAD DE EMPLEADOS
+                Console.WriteLine("Favor ingrese la cantidad de empleados.");
+                cantEmpleadoDigita = Console.ReadLine();
+
+                // ESPACIO
+                Console.WriteLine();
+
+                // VALIDACIONES 
+                if (int.TryParse(cantEmpleadoDigita, out cantEmpleados) && cantEmpleados > 0)
+                {
+                    // BUCLE PARA SOLICITAR SALARIO
+                    while (valorEmp++ < cantEmpleados)
+                    {
+                        // SOLICITA EL SALARIO DEL EMPLEADO
+                        Console.Write($"Favor ingrese el salario del empleado {valorEmp}: ");
+                        sueldoDigita = Console.ReadLine();
+
+                        // ESPACIO
+                        Console.WriteLine();
+
+                        // VALIDACIONES
+                        if (int.TryParse(sueldoDigita, out sueldo) && sueldo >= 100 && sueldo <= 500)
+                        {   
+                            // SUMA EL SUELDO DE CADA EMPLEADO
+                            gastNomina += sueldo;
+                        }
+                        else
+                        {
+                            // MENSAJE DE ERROR EN CASO SE INTRODUZCA UN VALOR NO VALIDO
+                            Console.WriteLine("Favor ingrese un sueldo mayor a 100$ y menor a 500$.");
+                        }
+
+                        // CALCULA LOS EMPLEADOS QUE GANAN ENTRE 100$ Y 300$
+                        if (sueldo <= 300)
+                        {
+                            // SI EL SUELDO ES MENOR A 300 INCREMENTA 1
+                            menor300++;
+                        }
+                        else
+                        {
+                            // SI EL SUELDO ES MAYOR A 300 INCREMENTA 1
+                            mayor300++;
+                        }
+
+                    }
+
+
+                }
+                else
+                {
+                    // MENSAJE DE ERROR EN CASO SE INTRODUZCA UN VALOR NO VALIDO
+                    Console.WriteLine("Favor ingrese valores validos.");
+                }
+
+                // MUESTRA LOS EMPLEADOS QUE GANAN ENTRE 100$ Y 300$ Y LOS QUE GANAN MAS DE 300$
+                Console.WriteLine($"Empleados que ganan entre 100$ y 300$: ({menor300})");
+                Console.WriteLine($"Empleados que ganan mas de 300$: ({mayor300})");
+                Console.WriteLine($"Gasto total en nomina: ({gastNomina}$)");
+
+
+            }
+            catch
+            {
+                // MENSAJE DE ERROR EN CASO SE INTRODUZCA UN VALOR NO VALIDO
+                Console.WriteLine("Favor ingrese valores validos.");
+            }
+
+        }
+    }
+}
